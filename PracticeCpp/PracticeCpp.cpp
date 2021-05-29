9a97e99a53487c160ca3ac433b6f3e86f7b96f85
Anjali Raghuvanshi <anjali7raghuvanshi@gmail.com>
2021-05-29 16:05:51 +0530

    input two no. then print

--- PracticeCpp/PracticeCpp.cpp
+++ PracticeCpp/PracticeCpp.cpp
@@ -3,9 +3,39 @@
 
 #include <iostream>
 
+/**
+* Taking two numbers as input from user and printing them
+*/
+void cinTwoValues()
+{
+    std::cout << "Enter two numbers separated by a space: ";
+
+    int x{ }; // define variable x to hold user input (and zero-initialize it)
+    int y{ }; // define variable y to hold user input (and zero-initialize it)
+    std::cin >> x >> y; // get two numbers and store in variable x and y respectively
+
+    std::cout << "You entered " << x << " and " << y << '\n';
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    //int a{};
+    //int b{};
+    //int c{};
+    //std::cin >> a;
+    //std::cin >> b;
+    //c = a + b;
+    //std::cout << "Hello World!\n";
+    //std::cout << "size of int " << sizeof(int) << std::endl;
+    //std::cout << "size of double " << sizeof(double) << std::endl;
+    //std::cout << "4";
+    //std::cout << 4;
+    //std::cout << "c " << c;
+
+    cinTwoValues();
+
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
