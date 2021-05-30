092c15792809ff4cc7900aa56468247a390aaab1
Anjali Raghuvanshi <anjali7raghuvanshi@gmail.com>
2021-05-30 17:18:38 +0530

    addition of uninitialized function

--- PracticeCpp/PracticeCpp.cpp
+++ PracticeCpp/PracticeCpp.cpp
@@ -3,6 +3,21 @@
 
 #include <iostream>
 
+void doNothing(int&) // Don't worry about what & is for now, we're just using it to trick the compiler into thinking variable x is used
+{
+}
+
+void uninitializedVar()
+{
+    // define an integer variable named x
+    int x; // this variable is uninitialized because we haven't given it a value
+
+    doNothing(x); // make the compiler think we're assigning a value to this variable
+
+    // print the value of x to the screen
+    std::cout << x; // who knows what we'll get, because x is uninitialized
+}
+
 /**
 * Taking two numbers as input from user and printing them
 */
@@ -32,7 +47,8 @@ int main()
     //std::cout << 4;
     //std::cout << "c " << c;
 
-    cinTwoValues();
+    //cinTwoValues();
+    uninitializedVar();
 
 
     return 0;
